2027d41d7b5131b9d45b5284b46ac614910b28ad
Daniel Erenshtein <daniel@bmybit.com>
2022-01-12 16:15:22 +0200

    Edited YandaGovernor quorum to be ranked

--- contract/contracts/YandaGovernor.sol
+++ contract/contracts/YandaGovernor.sol
@@ -1,20 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/governance/Governor.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+import "./custom_governance/Governor.sol";
+import "./custom_governance/extensions/GovernorSettings.sol";
+import "./custom_governance/extensions/GovernorCountingRanked.sol";
+import "./custom_governance/extensions/GovernorVotes.sol";
+import "./custom_governance/extensions/GovernorVotesQuorumFraction.sol";
+
 
 /// @custom:security-contact mariostumpo@bmybit.com
-contract YandaGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
-    constructor(ERC20Votes _token)
+contract YandaGovernor is Governor, GovernorSettings, GovernorCountingRanked, GovernorVotes, GovernorVotesQuorumFraction {
+    mapping(bytes => uint8) private _functionRankBySig;
+
+    constructor(IVotes _token,uint256 _votingDelay, uint256 _votingPeriod, uint256 _proposalThreshold, uint256[] memory _quorums)
         Governor("YandaGovernor")
-        GovernorSettings(120960 /* 1 week */, 34560 /* 2 days */, 100e18)
+        GovernorSettings(_votingDelay, _votingPeriod, _proposalThreshold)
         GovernorVotes(_token)
-        GovernorVotesQuorumFraction(4)
-    {}
+        GovernorVotesQuorumFraction(_quorums)
+    {
+        super.setFunctionRank(bytes(hex'2f2ff15d'), 2);  // grantRole
+        super.setFunctionRank(bytes(hex'd547741f'), 2);  // revokeRole
+        super.setFunctionRank(bytes(hex'3ac8a356'), 2);  // updateQuorumNumerator
+        super.setFunctionRank(bytes(hex'c0f99d67'), 1);  // addService
+    }
 
     // The following functions are overrides required by Solidity.
 
@@ -40,7 +48,7 @@ contract YandaGovernor is Governor, GovernorSettings, GovernorCountingSimple, Go
         public
         view
         override(IGovernor, GovernorVotesQuorumFraction)
-        returns (uint256)
+        returns (uint256[] memory)
     {
         return super.quorum(blockNumber);
     }
